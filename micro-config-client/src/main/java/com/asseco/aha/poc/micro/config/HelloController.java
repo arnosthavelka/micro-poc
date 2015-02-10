c18ac9d71715257acbe955d3717c4d1c61fe005e
Arnošt Havelka <arnost.havelka@gmail.com>
2015-02-10 13:02:41 +0100

    + [config] example of configuration DTO and refresh

--- micro-config-client/src/main/java/com/asseco/aha/poc/micro/config/HelloController.java
+++ micro-config-client/src/main/java/com/asseco/aha/poc/micro/config/HelloController.java
@@ -1,13 +1,17 @@
 package com.asseco.aha.poc.micro.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Autowired
+	private ConfigurationDTO cfg;
+
 	@RequestMapping("/hello")
 	String hello() {
-		return String.format("Hello!");
+		return String.format("Hello %s!", cfg.getName());
 	}
 }
