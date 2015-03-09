cb90765dfdf7258d2e876f160a6bd4eab7914939
Arnošt Havelka <arnost.havelka@gmail.com>
2015-03-09 13:05:38 +0100

    + update to Spring Boot 1.2.2 and Spring Cloud 1.0.0.RC3

--- micro-config-client/src/main/java/com/asseco/aha/poc/micro/config/ConfigClientApplication.java
+++ micro-config-client/src/main/java/com/asseco/aha/poc/micro/config/ConfigClientApplication.java
@@ -3,6 +3,9 @@ package com.asseco.aha.poc.micro.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Test URL: http://localhost:8088/env/config.client.name
+ */
 @SpringBootApplication
 public class ConfigClientApplication {
 
--- micro-config-server/src/main/java/com/asseco/aha/poc/micro/config/ConfigServerApplication.java
+++ micro-config-server/src/main/java/com/asseco/aha/poc/micro/config/ConfigServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Test URL: http://localhost:8888/mycfg/env
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
