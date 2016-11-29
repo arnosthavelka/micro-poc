4d40fbe8bef779931356e897b0fa4e324c071f04
Arnošt Havelka <arnost.havelka@gmail.com>
2016-11-29 11:04:25 +0100

    + update to SpringBoot 1.4.2, SpringBootAdmin 1.4.4 and SpringCloud Camden.SR2 (tested just on project micro-admin-server)

--- micro-location-district/src/test/java/com/github/aha/poc/micro/district/TestRepositoryApplication.java
+++ micro-location-district/src/test/java/com/github/aha/poc/micro/district/TestRepositoryApplication.java
@@ -4,16 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.github.aha.poc.micro.district.persistence.domain.District;
 import com.github.aha.poc.micro.district.persistence.repository.DistrictRepository;
 
 @Configuration
-@EntityScan(basePackageClasses = District.class)
 @EnableJpaRepositories(basePackageClasses = DistrictRepository.class)
 @Import({ DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, LiquibaseAutoConfiguration.class })
 public class TestRepositoryApplication {
