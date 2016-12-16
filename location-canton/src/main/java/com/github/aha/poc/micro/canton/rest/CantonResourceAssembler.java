350c63ba8907176f5d4e7762749e794cf69e3821
Arnošt Havelka <arnost.havelka@gmail.com>
2016-12-16 15:54:48 +0100

    + [location-canton] make district service name configurable

--- location-canton/src/main/java/com/github/aha/poc/micro/canton/rest/CantonResourceAssembler.java
+++ location-canton/src/main/java/com/github/aha/poc/micro/canton/rest/CantonResourceAssembler.java
@@ -3,6 +3,7 @@ package com.github.aha.poc.micro.canton.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.hateoas.Link;
@@ -13,6 +14,10 @@ import com.github.aha.poc.micro.canton.persistence.domain.Canton;
 
 @Component
 public class CantonResourceAssembler extends ResourceAssemblerSupport<Canton, CantonResource> {
+
+	// time to fetch available service is configurable by eureka.client.registryFetchIntervalSeconds (default is 30s)
+	@Value("${micro-poc.district.service-name:LOCATION-DISTRICT}")
+	private String districtServiceName;
 
 	/** Logger. */
 	private static final Logger LOG = LoggerFactory.getLogger(CantonResourceAssembler.class);
@@ -28,9 +33,7 @@ public class CantonResourceAssembler extends ResourceAssemblerSupport<Canton, Ca
 	public CantonResource toResource(final Canton entity) {
 		CantonResource cr = new CantonResource(entity);
 
-		// TODO: make it configurable in config server
-		// TODO: check timeout / cache configuration settings
-		dc.getInstances("LOCATION-DISTRICT").forEach(
+		dc.getInstances(districtServiceName).forEach(
 				(ServiceInstance s) -> {
 			String distrLink = String.format("%s/district/%s/", s.getUri(), entity.getDistrictCode());
 			LOG.debug("Adding district's link: {}", distrLink);
